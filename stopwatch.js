518745cef2902e7ef4ce687467a174d745822c33
HelenaYounes <younesq.helena@gmail.com>
2017-02-27 20:16:42 -0700

    add stopbutton

--- stopwatch.js
+++ stopwatch.js
@@ -1,1 +1,16 @@
-alert('hello');
+var start;
+var onLoad = function(){
+
+  start = setInterval(increment, 1000);
+  var buttonstop = document.querySelector('.stop');
+  buttonstop.addEventListener('click', stop);
+}
+
+function increment(){
+  var object= document.querySelector('.counter');
+  object.innerHTML++;
+}
+function stop(){
+  clearInterval(start);
+}
+window.addEventListener('load', onLoad);
